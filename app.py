4d021755d6804993704241244dc2239886625c15
dacasfilipe <filiperdacas@gmail.com>
2023-09-18 11:01:22 -0300

    update app.py

--- app.py
+++ app.py
@@ -2,10 +2,19 @@
 from flask import Flask
 # config import
 from config import app_config, app_active
+config = app_config[app_active]
+from flask_sqlalchemy import SQLAlchemy
 
 def create_app(config_name):
     app = Flask(__name__, template_folder='templates')
+    app.config.from_pyfile('config.py')
+
     config_object = app_config[config_name]
+    app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
+    app.config['SALALCHEMY_TRACK_MODIFICATIONS'] = False
+    db = SQLAlchemy(config.APP)
+    db.init_app(app)
+
 
     if config_object is None:
         raise Exception('No configuration found for the current environment.')
